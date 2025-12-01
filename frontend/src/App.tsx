bd96a785cc7eccb7b1e736466fc9dfc264ea1937
Starco3000 <minhnguyen9686q6@gmail.com>
2025-12-01 15:55:11 +0700

    feat: Create a SignUp UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { Toaster } from 'sonner';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+import ChatAppPage from './pages/ChatAppPage';
 
 function App() {
-
   return (
     <>
-    Hello World
+      <Toaster richColors />
+      <BrowserRouter>
+        <Routes>
+          {/* Public routes */}
+          <Route path='/signin' element={<SignInPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+
+          {/* Protect routes */}
+          <Route path='/' element={<ChatAppPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
