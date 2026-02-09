ba5502ff4f985fc5423d9f6053d7ff0c4480f06d
Starco3000 <minhnguyen9686q6@gmail.com>
2026-02-10 01:23:16 +0700

    feat: Add Middleware to protect socket

--- backend/src/socket/index.js
+++ backend/src/socket/index.js
@@ -1,6 +1,7 @@
 import { Server } from 'socket.io';
 import http from 'http';
 import express from 'express';
+import { socketAuthMiddleware } from '../middlewares/socketMiddleware.js';
 
 const app = express();
 
@@ -13,8 +14,11 @@ const io = new Server(server, {
   },
 });
 
+io.use(socketAuthMiddleware);
+
 io.on('connection', async (socket) => {
-  console.log(`socket connection: ${socket.id}`);
+  const user = socket.user;
+  console.log(`${user.displayName} online with socket ${socket.id}`);
 
   socket.on('disconnect', () => {
     console.log(`socket disconnected: ${socket.id}`);
